37bed85d5dd918d9005dde9ee22f804628d20e4b
Daniel Holmer <daniel.holmer@liu.se>
2020-11-27 19:41:30 +0100

    drafts

--- raft/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Hello!",
-        "excerpt":"Finishing my bachelor and going into my masters degree, I felt like I needed a place to share my ongoing projects. I’ve made earlier attempts 1 at creating a personal website, which never really got completed. When I stumbled upon the Ruby-based Jekyll, I decided to give it another shot....","categories": [],
-        "tags": ["life update"],
-        "url": "http://localhost:4000/first-post/",
-        "teaser": null
-      }]
